175e8f59b98187c659a1583dd4a53471c558c8fa
Adnan Sheikh <adnan-sheikh@pop-os.localdomain>
2023-10-07 05:30:12 +0530

    add get and update preferences handlers

--- src/handler/preferences.js
+++ src/handler/preferences.js
@@ -1,13 +1,15 @@
 import { db } from "../db/index.js";
 
-export function getPreferences() {}
+export function getPreferences(req, res) {
+  const { username } = req.user;
+  const userFromDB = db.users[username];
+  res.json(userFromDB.preferences);
+}
 
-export function createPreferences(req, res) {
+export function createOrUpdatePreferences(req, res) {
   const { username } = req.user;
   const userFromDB = db.users[username];
   const preferences = req.body;
   db.users[username] = { ...userFromDB, preferences };
   res.json(preferences);
 }
-
-export function updatePreferences() {}
--- src/router.js
+++ src/router.js
@@ -1,17 +1,16 @@
 import express from "express";
 import {
-  createPreferences,
+  createOrUpdatePreferences,
   getPreferences,
-  updatePreferences,
 } from "./handler/preferences.js";
 import { validatePreferences } from "./middlewares/validatePreferences.js";
 import { db } from "./db/index.js";
 
 export const router = express.Router();
 
-router.post("/preferences", validatePreferences, createPreferences);
+router.post("/preferences", validatePreferences, createOrUpdatePreferences);
 router.get("/preferences", getPreferences);
-router.put("/preferences", validatePreferences, updatePreferences);
+router.put("/preferences", validatePreferences, createOrUpdatePreferences);
 
 router.get("/users", (req, res) => {
   // @ts-ignore
