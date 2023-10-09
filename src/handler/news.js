b3a7972518bdaeeef2c0dfa2695f4a5d15ffde84
Adnan Sheikh <adnan-sheikh@pop-os.localdomain>
2023-10-09 12:33:50 +0530

    refactor

--- src/handler/news.js
+++ src/handler/news.js
@@ -3,6 +3,25 @@ import { MAX_TTL_FOR_ARTICLES } from "../const/index.js";
 import { db } from "../db/index.js";
 import { newsAPI } from "../externalAPI/newsAPI.js";
 
+function isArticleInCacheExpired(preferences) {
+  const { data, setTime } = cache.get(preferences);
+  const elapsedTime = Date.now() - setTime;
+  return { isArticleExpired: elapsedTime <= MAX_TTL_FOR_ARTICLES, data };
+}
+
+async function fetchNewsAndUpdateCache(preferences) {
+  try {
+    const news = await newsAPI.get("/top-headlines", {
+      params: preferences,
+    });
+    cache.set(preferences, { data: news.data, setTime: Date.now() });
+    return news.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error("There was an error fetching news on our server.");
+  }
+}
+
 export async function getNews(req, res) {
   const { username } = req.user;
   const userFromDB = db.users[username];
@@ -13,30 +32,21 @@ export async function getNews(req, res) {
         "There are no preferences set for fetching news. Please set preferences first!",
     });
   }
+
   try {
     if (!cache.has(preferences)) {
-      const news = await newsAPI.get("/top-headlines", {
-        params: preferences,
-      });
-      cache.set(preferences, { data: news.data, setTime: Date.now() });
-      return res.json(news.data);
+      const news = await fetchNewsAndUpdateCache(preferences);
+      return res.json(news);
     }
 
-    const { data, setTime } = cache.get(preferences);
-    const elapsedTime = Date.now() - setTime;
-    if (elapsedTime <= MAX_TTL_FOR_ARTICLES) {
+    const { data, isArticleExpired } = isArticleInCacheExpired(preferences);
+    if (isArticleExpired) {
       return res.json(data);
     }
 
-    const news = await newsAPI.get("/top-headlines", {
-      params: preferences,
-    });
-    cache.set(preferences, { data: news.data, setTime: Date.now() });
-    return res.json(news.data);
-  } catch (e) {
-    console.log(e);
-    res
-      .status(500)
-      .json({ error: "There was an error fetching news on our server." });
+    const news = await fetchNewsAndUpdateCache(preferences);
+    return res.json(news);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 }
